08014cfecc157434b422cb0564f5d2d42376e13b
Jemal <jamalsamba@gmail.com>
2024-09-01 15:13:54 +0200

    fix #39

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import React from 'react';
 
 const LoginForm = () => {
@@ -37,7 +37,7 @@ const LoginForm = () => {
     }, []);
 
     if (!isModalOpen) return null;
-    
+
         // Create user object
         const userData = {
             email: email,
