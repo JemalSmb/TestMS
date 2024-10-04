769c4446798faaa0b9effe649ef11d31485102e9
Jemal <jamalsamba@gmail.com>
2024-10-04 16:43:32 +0100

    fix #115

--- src/components/ads/AdHandling.js
+++ src/components/ads/AdHandling.js
@@ -113,7 +113,7 @@ const AdHandler = () => {
           <h className="text-black text-4xl font-bold">We're offering the best</h> <h className="text-4xl font-bold text-pink-500">Services</h>
           <div>
           <h className="text-black text-4xl pt-4 font-bold">Featured Videos</h> 
-          <div className="ads-container flex-grow grid gap-4 justify-center overflow-y-auto" style={{ gridTemplateColumns: `repeat(auto-fill, minmax(250px, 1fr))` }}>
+          <div className="ads-container flex-grow gap-4 flex-wrap flex justify-center overflow-y-auto" style={{ gridTemplateColumns: `repeat(auto-fill, minmax(250px, 1fr))` }}>
           {ads.slice(0, 4).map((ad, index) => (
                         <div
                             className={`ad relative rounded-xl border-5 border-y-cyan-500 shadow-md overflow-hidden cursor-pointer ${getAspectRatioClass(ad)}`}
