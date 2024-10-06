dcbace3b9a7e08d20d89bc32b3199979c1c24618
Jemal <jamalsamba@gmail.com>
2024-10-06 22:59:56 +0100

    fix #193

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -105,8 +105,7 @@ const NavBar = ({ DarkMode, toggleDarkMode }) => {
                                 &times;
                             </button>
                             <ul className="py-1 mt-8">
-                                <li><a className="block px-4 py-2 text-lg text-gray-400" 
-                                 onClick={toggleMenu()}>{Connect.link}</a></li>
+                                <li><a className="block px-4 py-2 text-lg text-gray-400" >{Connect.link}</a></li>
                                 <li><a className="block px-4 py-2 text-lg text-gray-400"   
                                 onClick={() => { navigate('Dashboard'); toggleMenu();}}>Dashboard</a></li>
                                 <li><a className="block px-4 py-2 text-lg text-gray-400"
